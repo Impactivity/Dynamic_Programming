4e26d4947793d53ea0daf177a3492d9859927b64
Impactivity <minwoo2944@gmail.com>
2022-03-21 17:33:37 +0900

    3.21

--- triangle.py
+++ triangle.py
@@ -1,46 +1,11 @@
-# _max = 0
-#
-#
-# def dfs(inx, j, tot, triangle):
-#     global _max
-#     if inx == len(triangle):
-#         _max = max(_max, tot)
-#         return
-#
-#     for i in range(j, j + 2):
-#         if i < len(triangle[inx]):
-#             tmp = dfs(inx + 1, i, tot + triangle[inx][i], triangle)
-#
-#
-# def solution(triangle):
-#     dfs(0, 0, 0, triangle)
-#
-#     return
-
-
-from collections import deque
-
-
 def solution(triangle):
-    queue = deque()
-    visited = [[0] * i for i in range(1, len(triangle) + 1)]
-    queue.append((0, 0))
-    visited[0][0] = triangle[0][0]
-    dx = [1, 1]
-    dy = [0, 1]
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(2):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < len(triangle):
-                if 0 <= ny < len(triangle[nx]):
-                    if visited[nx][ny] == 0:
-                        visited[nx][ny] = visited[x][y] + triangle[nx][ny]
-                        queue.append((nx, ny))
-
-    print(visited)
-    print(max(visited[len(triangle) - 1]))
-
-    return
+    for i in range(1,len(triangle)):
+        n = len(triangle[i])
+        for j in range(n):
+            if j == 0 :
+                triangle[i][j] += triangle[i-1][j]
+            elif j == n - 1:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1],triangle[i-1][j])
+    return max(triangle[len(triangle)-1])
